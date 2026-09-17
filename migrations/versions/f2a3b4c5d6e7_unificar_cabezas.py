a15dcd926ff9e8d161259b9cd94243eaf7e509a2
FdezDev <fdeznet224@gmail.com>
2026-09-16 19:08:23 -0600

    release: preparar version 2.18.4

---
+++ migrations/versions/f2a3b4c5d6e7_unificar_cabezas.py
@@ -0,0 +1,19 @@
+"""unifica las ramas de migraciones previas
+
+Revision ID: f2a3b4c5d6e7
+Revises: e1f2a3b4c5d6, f4a5b6c7d8e9
+Create Date: 2026-09-16
+"""
+
+revision = "f2a3b4c5d6e7"
+down_revision = ("e1f2a3b4c5d6", "f4a5b6c7d8e9")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
